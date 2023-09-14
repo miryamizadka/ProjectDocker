494babf655cde6233c9febbfe1ac150240be1562
rivkyschon <rivkyschon@outlook.co.il>
2023-09-14 09:46:24 +0300

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,11 @@
-docker build -t  my-chat-app . -f bonus.dockerfile 
-docker run -p 5000:5000 --cpus "2.0" --memory "1g" my-chat-app
+# docker build -t  my-chat-app .
+# docker run -p 5000:5000 --cpus "2.0" --memory "1g" my-chat-app
+
+# Prompt the user for the version
+read -p "Enter the version to build and run (e.g., v1): " VERSION
+
+# # Build the Docker image with the specified version
+# docker build -t my-chat-app:$VERSION .
+
+# Run the Docker container with the specified version
+docker run -p 5000:5000 --cpus "2.0" --memory "1g" my-chat-app:$VERSION
